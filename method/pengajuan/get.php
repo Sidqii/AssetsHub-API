20b1e20ea114030251628711a25dfdc706d66c48
Sidqii <SidqiMhmmd412@gmail.com>
2025-05-21 20:24:04 +0700

    update method

--- method/pengajuan/get.php
+++ method/pengajuan/get.php
@@ -20,6 +20,28 @@ function ambilPengajuan($conn)
         } else {
             echo json_encode(['error' => 'Barang tidak ditemukan']);
         }
+    } else if (isset($_GET['id_pengguna'])) {
+        $query = 'SELECT p.id, p.id_pengguna, s.kode, u.username, p.instansi, p.hal, i.nama_barang, p.jumlah, p.tgl_kembali
+                  FROM pengajuan p
+                  LEFT JOIN items i ON p.id_barang = i.id
+                  LEFT JOIN users u ON p.id_pengguna = u.id
+                  LEFT JOIN status s ON p.id_status = s.id
+                  WHERE p.id_pengguna = ?';
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('i', $_GET['id_pengguna']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $idPengajuan = [];
+            while ($row = $result->fetch_assoc()) {
+                $idPengajuan[] = $row;
+            }
+            echo json_encode($idPengajuan);
+        } else {
+            echo json_encode([]);
+        }
     } else {
         $query = 'SELECT p.id, s.kode, u.username, p.instansi, p.hal, i.nama_barang, p.jumlah, p.tgl_kembali
                   FROM pengajuan p
